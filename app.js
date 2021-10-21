805ec4aecdc0c681f35157817dbbe86646dd96fa
Rick Miller <rmillz03@gmail.com>
2021-10-21 16:49:14 -0400

    added express

--- app.js
+++ app.js
@@ -1,15 +1,37 @@
-// create an express app
-const express = require("express")
-const app = express()
+if (process.env.NODE_ENV !== 'production') {
+  require('dotenv').config()
+}
 
-// use the express-static middleware
+const express = require("express");
+const expressLayouts = require('express-ejs-layouts');
+const morgan = require('morgan');                       //logs requests directed to backend
+const app = express();
+const port = process.env.PORT || 8080;
+
+app.use(morgan('dev'))
+
+app.set('view engine', 'ejs')
+app.set('views', __dirname + '/views')
+app.set('layout', 'layouts/mainLayout')
+app.use(expressLayouts)
 app.use(express.static("public"))
+app.use(express.urlencoded({ extended: false })) //need to allow pages to access form data
 
-// define the first route
-app.get("/", function (req, res) {
-  res.send("<h1>Hello World!</h1>")
-})
+//----------- Routes -----------
+// const userRoute = require('./routes/userRoute');
+// app.use('/userRoute', userRoute);
+// const flightRoute = require('./routes/flightRoute');
+// app.use('/flightRoute', flightRoute);
+// const crewRoute = require('./routes/crewRoute');
+// app.use('/crewRoute', crewRoute);
+// const planeRoute = require('./routes/planeRoute');
+// app.use('/planeRoute', planeRoute);
 
-// start the server listening for requests
-app.listen(process.env.PORT || 3000, 
-	() => console.log("Server is running..."));
+
+//----------- Get the Party Started! -----------
+app.get('/', (req, res) => {
+    //res.redirect('/userRoute/login');
+    res.render('/users/login.ejs');
+});
+ 
+app.listen(port, () => console.log("Server is running..."));
